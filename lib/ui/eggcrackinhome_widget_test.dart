2470df51d7aac3a68dbb2ca5373bb2aeea858c80
mswift42 <martin.haesler@gmail.com>
2018-02-27 18:40:19 +0000

    Add EggCrackinHome file + test.

---
+++ lib/ui/eggcrackinhome_widget_test.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_test/flutter_test.dart';
+
+import 'package:fleggcrackin/ui/EggCrackinHome.dart';
+
+void main() {
+  testWidgets('EggCrackinHome shows home page', (WidgetTester tester) async {
+
+  });
+}
+
+
