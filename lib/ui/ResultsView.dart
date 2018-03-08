523964bc3f07c461acb0dd0e8681742134693079
mswift42 <martin.haesler@gmail.com>
2018-03-08 16:30:41 +0000

    import Recipe class.

--- lib/ui/ResultsView.dart
+++ lib/ui/ResultsView.dart
@@ -1,3 +1,16 @@
 import 'package:flutter/material.dart';
+import 'package:fleggcrackin/services/Recipe.dart' show Recipe;
 
+class ResultsView extends StatefulWidget {
+  @override
+  _ResultsViewState createState() => new _ResultsViewState();
+}
+
+class _ResultsViewState extends State<ResultsView> {
+  List<Recipe> _recipes = new List<Recipe>();
+  @override
+  Widget build(BuildContext context) {
+    return new Container();
+  }
+}
 
